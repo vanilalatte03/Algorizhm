e509b80c0ac1ecda47f5723cd6b117b13f4660e1
JH <126761060+vanilalatte03@users.noreply.github.com>
2024-08-29 16:25:13 +0900

    [Bronze V] Title: ??!, Time: 104 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        String ls = "??!";
+       
+        System.out.println(str+ls);
+  
+        
+    }
+}
