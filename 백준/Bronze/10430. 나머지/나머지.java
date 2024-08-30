1943d1909549c8ff748579dfcbcab6f8e1012fb0
JH <126761060+vanilalatte03@users.noreply.github.com>
2024-08-30 10:45:11 +0900

    [Bronze V] Title: 나머지, Time: 100 ms, Memory: 14268 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+    
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String[] s = br.readLine().split(" ");
+        int a = Integer.parseInt(s[0]);
+        int b = Integer.parseInt(s[1]);
+        int c = Integer.parseInt(s[2]);
+        
+        System.out.println((a+b)%c);
+        System.out.println(((a%c)+(b%c))%c); 
+        System.out.println((a*b)%c);
+        System.out.println(((a%c)*(b%c))%c);
+    }
+}
