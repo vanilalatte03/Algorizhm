613783eb1378e72826796874f2707ecd875f4d25
JH <126761060+vanilalatte03@users.noreply.github.com>
2024-09-05 16:19:01 +0900

    [Bronze V] Title: A+B - 8, Time: 140 ms, Memory: 16532 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int T = Integer.parseInt(br.readLine());
+        
+        for(int i = 1; i <= T; i++){
+            String[] input = br.readLine().split(" ");
+            int a = Integer.parseInt(input[0]);
+            int b = Integer.parseInt(input[1]);
+            
+            bw.write("Case #" + i + ": " + a + " + " + b + " = "+ (a+b)+"\n");
+        }
+        
+        bw.flush();
+        bw.close();
+        
+    }
+}
