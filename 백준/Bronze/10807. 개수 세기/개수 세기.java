ad383ae35134e9794d2d11c4f4811fd99b12368f
JH <126761060+vanilalatte03@users.noreply.github.com>
2024-09-11 23:09:27 +0900

    [Bronze V] Title: 개수 세기, Time: 100 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -7,13 +7,10 @@ public class Main{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int count = 0;
         
-        // 정수의 개수 입력
         int N = Integer.parseInt(br.readLine()); 
         
-        // N개의 정수를 공백으로 구분하여 입력받음
         String[] inputs = br.readLine().split(" ");
         
-        // 찾으려는 정수 v 입력
         int v = Integer.parseInt(br.readLine());
         
         // inputs 배열을 순회하면서 v와 같은 값을 찾음
@@ -23,7 +20,6 @@ public class Main{
             }
         }
         
-        // 결과 출력
         System.out.println(count);
     }
 }
