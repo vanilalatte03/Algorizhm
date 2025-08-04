d1e9e82c19bf9a4700707446e36253363658c0c6
JH <126761060+vanilalatte03@users.noreply.github.com>
2025-08-04 17:13:34 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 108 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
@@ -1,19 +1,14 @@
 import java.io.*;
 import java.util.*;
-
 public class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String original = br.readLine();
-
-        StringBuilder sb = new StringBuilder(original);
-        sb.reverse();
-        String rv = sb.toString();
-
-        if (original.equals(rv)) {
-            System.out.println("1");
+        String s = br.readLine();
+        StringBuilder sb = new StringBuilder(s);
+        if (s.equals(sb.reverse().toString())) {
+            System.out.print(1);
         } else {
-            System.out.println("0");
+            System.out.print(0);
         }
     }
 }
