22352615ac90961fda9d2eab67854b4385650a54
JH <126761060+vanilalatte03@users.noreply.github.com>
2025-11-08 00:50:45 +0900

    [Silver I] Title: 미로 탐색, Time: 108 ms, Memory: 14744 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,35 +1,34 @@
-import java.io.*;
 import java.util.*;
-
-class Point{
+import java.io.*;
+class Point {
     int x;
     int y;
-
     public Point(int x, int y){
         this.x = x;
         this.y = y;
     }
 }
 
-public class Main {
+public class Main{
     static int N, M;
     static int[][] map;
     static boolean[][] visited;
 
-    //방향 배열
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
 
     public static void main(String[] args) throws Exception{
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        M = sc.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
         map = new int[N][M];
         visited = new boolean[N][M];
 
         for(int i = 0; i < N; i++){
-            String str = sc.next();
+            String str = br.readLine();
             for(int j = 0; j < M; j++){
                 map[i][j] = str.charAt(j) - '0';
             }
@@ -38,7 +37,6 @@ public class Main {
         bfs(0,0);
 
         System.out.println(map[N-1][M-1]);
-
     }
 
     public static void bfs(int X, int Y){
@@ -57,7 +55,7 @@ public class Main {
                 int nextX = currentX + dx[i];
                 int nextY = currentY + dy[i];
 
-                if(nextX < 0 || nextX >= N || nextY < 0 || nextY >= M){
+                if(nextX < 0 || nextY < 0 || nextX >= N || nextY >= M){
                     continue;
                 }
 
@@ -71,9 +69,10 @@ public class Main {
 
                     map[nextX][nextY] = map[currentX][currentY] + 1;
                 }
+
+
             }
 
         }
-
     }
 }
