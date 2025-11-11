cc6cf124a44ac5d7434d149aac94f0021cfdb3ff
JH <126761060+vanilalatte03@users.noreply.github.com>
2025-11-11 23:48:30 +0900

    [Silver I] Title: 숨바꼭질, Time: 204 ms, Memory: 26568 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+public class Main{
+    static int N, K;
+    static int[] time;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws Exception{
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        K = sc.nextInt();
+
+        time = new int[1000001];
+        visited = new boolean[1000001];
+
+        bfs(N);
+
+        System.out.println(time[K]);
+    }
+
+    public static void bfs(int StartPos){
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(StartPos);
+        visited[StartPos] = true;
+
+        while(!queue.isEmpty()){
+            int currentPos = queue.poll();
+
+            int next1 = currentPos - 1;
+            if (next1 >= 0 && !visited[next1]) {
+                queue.add(next1);
+                visited[next1] = true;
+                time[next1] = time[currentPos] + 1;
+            }
+
+            int next2 = currentPos + 1;
+            if (next2 <= 100000 && !visited[next2]) {
+                queue.add(next2);
+                visited[next2] = true;
+                time[next2] = time[currentPos] + 1;
+            }
+
+            int next3 = currentPos * 2;
+            if (next3 <= 100000 && !visited[next3]) {
+                queue.add(next3);
+                visited[next3] = true;
+                time[next3] = time[currentPos] + 1;
+            }
+
+        }
+
+    }
+}
