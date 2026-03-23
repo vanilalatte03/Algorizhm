2db79480cf280a4e9d6bffaa8d5b21be65cd0552
Jiho Lim <126761060+vanilalatte03@users.noreply.github.com>
2026-03-23 10:38:36 +0900

    [level 2] Title: 기능개발, Time: 3.54 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,30 +1,15 @@
 import java.util.*;
+
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        List<Integer> list = new ArrayList<>();
-
-        int firstEndDay = (100 - progresses[0] + speeds[0] - 1) / speeds[0];
-        int bundle = 1;
-
-        for (int i = 1; i < progresses.length; i++) {
-            int endDay = (100 - progresses[i] + speeds[i] - 1) / speeds[i];
-
-            if (endDay <= firstEndDay) {
-                bundle++;
-            } else {
-                list.add(bundle);
-                firstEndDay = endDay;
-                bundle = 1;
+        int[] dayOfend = new int[100];
+        int day = -1;
+        for(int i=0; i<progresses.length; i++) {
+            while(progresses[i] + (day*speeds[i]) < 100) {
+                day++;
             }
+            dayOfend[day]++;
         }
-
-        list.add(bundle);
-
-        int[] answer = new int[list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            answer[i] = list.get(i);
-        }
-
-        return answer;
+        return Arrays.stream(dayOfend).filter(i -> i!=0).toArray();
     }
 }
