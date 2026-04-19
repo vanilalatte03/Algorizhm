62cc45b2e6edc0949868bcbe25613fff3ab2d159
Jiho Lim <126761060+vanilalatte03@users.noreply.github.com>
2026-04-19 23:46:59 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 85.05 ms, Memory: 97.5 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,9 +1,9 @@
-import java.util.HashMap;
+import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        String answer= "";
-        HashMap<String, Integer> map = new HashMap<>();
+        Map<String, Integer> map = new HashMap<>();
+        
         for(String name : participant) {
             map.put(name, map.getOrDefault(name, 0) + 1);
         }
@@ -12,13 +12,16 @@ class Solution {
             map.put(name, map.get(name) - 1);
         }
         
-        for(String name : map.keySet()){
-            if(map.get(name) != 0){
-                answer = name;
-                break;
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if (entry.getValue() != 0) {
+                return entry.getKey();
             }
         }
 
-        return answer;
+        return "";
     }
 }
+
+//1. 참가자 배열을 돌면서 +1
+//2. 완주자 배열을 돌면서 -1
+//3. map을 순회하면서 value가 0이 아니면 그 이름이 완주하지 못한 선수
