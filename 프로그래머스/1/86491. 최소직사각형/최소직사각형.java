2da271665e5301050c6ffae96551c73fee8c8a7b
Jiho Lim <126761060+vanilalatte03@users.noreply.github.com>
2026-05-05 23:10:07 +0900

    [level 1] Title: 최소직사각형, Time: 2.46 ms, Memory: 93.5 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -1,22 +1,16 @@
-import java.util.*;
-
 class Solution {
     public int solution(int[][] sizes) {
-        int maxW = 0;
-        int maxH = 0;
-        
-        for(int i = 0; i < sizes.length; i++){
-            int w = Math.max(sizes[i][0], sizes[i][1]);
-            int h = Math.min(sizes[i][0], sizes[i][1]);
-            
-            maxW = Math.max(maxW, w);
-            maxH = Math.max(maxH, h);
+        int maxWidth = 0;
+        int maxHeight = 0;
+
+        for (int[] size : sizes) {
+            int width = Math.max(size[0], size[1]);
+            int height = Math.min(size[0], size[1]);
+
+            maxWidth = Math.max(maxWidth, width);
+            maxHeight = Math.max(maxHeight, height);
         }
-        
-        return maxW * maxH;
+
+        return maxWidth * maxHeight;
     }
 }
-
-//1. 큰값을 가로, 작은값을 세로로 정렬한다.
-//2. 세로, 가로 각각 정렬된 값중 가장 큰값을 찾는다.
-//3. 두 값을 곱해서 리턴한다.
