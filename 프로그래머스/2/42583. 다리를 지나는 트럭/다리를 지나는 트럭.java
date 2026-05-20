72e643e1eb27396d3bbe54901ad7ae25a9eac9d4
Jiho Lim <126761060+vanilalatte03@users.noreply.github.com>
2026-05-20 09:58:41 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 9.33 ms, Memory: 60.2 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -1,32 +1,44 @@
 import java.util.*;
 
 class Solution {
-    public int solution(int bridge_length, int weight, int[] truck_weights) {        
-        Deque<Integer> bridge = new ArrayDeque<>();
-        
-         for (int i = 0; i < bridge_length; i++) {
-            bridge.offerLast(0);
+
+    static class Truck {
+        int weight;
+        int exitTime;
+
+        Truck(int weight, int exitTime) {
+            this.weight = weight;
+            this.exitTime = exitTime;
         }
-        
+    }
+
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
+        Queue<Truck> bridge = new ArrayDeque<>();
+
         int time = 0;
         int currentWeight = 0;
         int truckIndex = 0;
-        
-        while (truckIndex < truck_weights.length) {
+
+        while (truckIndex < truck_weights.length || !bridge.isEmpty()) {
             time++;
 
-            currentWeight -= bridge.pollFirst();
+            // 현재 시간에 다리를 빠져나갈 트럭 제거
+            if (!bridge.isEmpty() && bridge.peek().exitTime == time) {
+                currentWeight -= bridge.poll().weight;
+            }
 
-            if (currentWeight + truck_weights[truckIndex] <= weight) {
-                bridge.offerLast(truck_weights[truckIndex]);
-                currentWeight += truck_weights[truckIndex];
+            // 다음 트럭을 올릴 수 있으면 올림
+            if (truckIndex < truck_weights.length
+                    && currentWeight + truck_weights[truckIndex] <= weight) {
+
+                int truckWeight = truck_weights[truckIndex];
+
+                bridge.offer(new Truck(truckWeight, time + bridge_length));
+                currentWeight += truckWeight;
                 truckIndex++;
-            } else {
-                bridge.offerLast(0);
             }
         }
 
-        return time + bridge_length;
-        
+        return time;
     }
 }
